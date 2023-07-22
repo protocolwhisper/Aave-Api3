a7331dad165f52ac0e1d9d2941856c088b173a13
protocolwhisper <chrismatai@hotmail.com>
2023-07-22 13:10:23 -0500

    (rebase):Fixing Test1

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import {load } from "ts-dotenv";
+import { string } from "hardhat/internal/core/params/argumentTypes";
 
+const env = load ({
+  API_KEY : String, 
+})
 const config: HardhatUserConfig = {
   solidity: "0.8.10",
+  networks: {
+    hardhat: {
+      forking: {
+        url: env.API_KEY,
+      }
+    }
+  }
 };
+
+
 
 export default config;
--- test/Impersonating.ts
+++ test/Impersonating.ts
@@ -8,36 +8,23 @@ import hre from "hardhat";
 import Address from "@typechain/ethers-v5"
 
 
-describe("Aave-Api3 - Unit tests" , function(){
+describe("Aave-Api3 - Unit tests", function() {
     let Pool_providerFactory: ContractFactory;
-    let pool_contract : PoolAddressesProvider;
-    let accounts:  SignerWithAddress[];
-    let impersonate_signer : SignerWithAddress;
-
+    let pool_contract: PoolAddressesProvider;
+    let accounts: SignerWithAddress[];
+    let impersonate_signer: SignerWithAddress;
+    let old_owner: string;
+  
     before(async function() {
         let pool_provider_owner = "0xfA0e305E0f46AB04f00ae6b5f4560d61a2183E00";
-        impersonate_signer = await ethers.getImpersonatedSigner(pool_provider_owner)
-        Pool_providerFactory = await ethers.getContractFactory("PoolAddressesProvider");
+        impersonate_signer = await ethers.getSigner(pool_provider_owner);
+        pool_contract = await ethers.getContractAt("PoolAddressesProvider", "0x0496275d34753A48320CA58103d5220d394FF77F", impersonate_signer);
     });
 
-    beforeEach(async function(){
-        pool_contract = await Pool_providerFactory.deploy("Ethereum Aave Marke" , impersonate_signer.address) as PoolAddressesProvider;
-        
-   });
-
-    describe("Test control over contract" , function() {
-        it("Owner of pool provider must be local signer" , async function() {
-            let old_owner = "0xfA0e305E0f46AB04f00ae6b5f4560d61a2183E00";
-            let owner = pool_contract.owner();
-
-            expect(owner).to.not.equal(old_owner , "Owner must be local signer")
-            
-            
-
+    describe("Test control over contract", function() {
+        it("Owner of pool provider must be local signer", async function() {
+            let owner = await pool_contract.owner();
+            expect(owner).to.be.equal(impersonate_signer.address, "Owner must be local signer");
         });
-
     });
-   
-
-   
-})
+});
