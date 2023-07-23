f1b19aa3965a5118278a6a17ca03c419bfcdc408
protocolwhisper <chrismatai@hotmail.com>
2023-07-22 20:44:33 -0500

    (feature): Fully functional PoC

--- contracts/formatOracle.sol
+++ contracts/formatOracle.sol
@@ -1,41 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import "@aave/core-v3/contracts/interfaces/IPriceOracleGetter.sol";
 import {IDataFeedApi3} from "./IApi3.sol";
 
-contract MyPriceOracle is IPriceOracleGetter {
+contract WETHapioracle {
     IDataFeedApi3 public oracle;
     address public api3oracle;
-    address public base_currency;
-    uint256 public base_currency_unit;
 
     // We gave our API3 proxy address
     constructor(address _dataFeedReaderAddress) {
         _api3oracle(_dataFeedReaderAddress);
     }
 
-    function BASE_CURRENCY() external view override returns (address) {
-        return base_currency;
-        // return
-    }
-
-    function BASE_CURRENCY_UNIT() external view override returns (uint256) {
-        // return the base currency unit
-        return base_currency_unit;
-    }
-
     function _api3oracle(address fallbackOracle) internal {
         oracle = IDataFeedApi3(fallbackOracle);
     }
 
-    function getAssetPrice(address asset) external view returns (uint256) {
-        (int224 price, uint256 timestamp) = oracle.readDataFeed();
+    function latestAnswer() external view returns (int256) {
+        (int224 price, ) = oracle.readDataFeed();
         require(price >= 0, "Negative price is not allowed");
 
-        // Converting int224 to uint256
-        uint256 positivePrice = uint256(int256(price));
+        // Converting int224 to int256
+        int256 positivePrice = int256(price);
 
         return positivePrice;
+    }
+
+    function getTokenType() external pure returns (uint256) {
+        return 1;
+    }
+
+    function decimals() external pure returns (uint8) {
+        return 8;
     }
 }
--- test/Impersonating.ts
+++ test/Impersonating.ts
@@ -2,10 +2,11 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect, should }   from "chai";
 import { ContractFactory } from "ethers";
 import { ethers }   from "hardhat";
-import { PoolAddressesProvider } from "../typechain-types";
+import { PoolAddressesProvider, AaveOracle , WETHapioracle__factory, aave } from "../typechain-types";
 import { timeStamp } from "console";
 import hre from "hardhat";
 import Address from "@typechain/ethers-v5"
+import { Contract } from "hardhat/internal/hardhat-network/stack-traces/model";
 
 
 describe("Aave-Api3 - Unit tests", function() {
@@ -13,12 +14,25 @@ describe("Aave-Api3 - Unit tests", function() {
     let pool_contract: PoolAddressesProvider;
     let accounts: SignerWithAddress[];
     let impersonate_signer: SignerWithAddress;
-    let old_owner: string;
+    let aave_oracle : AaveOracle;
+
   
     before(async function() {
+        // ACL Manager Contract
+        accounts = await ethers.getSigners()
         let pool_provider_owner = "0xfA0e305E0f46AB04f00ae6b5f4560d61a2183E00";
+        await hre.network.provider.request({
+            method : "hardhat_impersonateAccount",
+            params: [pool_provider_owner],
+        });
+      
         impersonate_signer = await ethers.getSigner(pool_provider_owner);
         pool_contract = await ethers.getContractAt("PoolAddressesProvider", "0x0496275d34753A48320CA58103d5220d394FF77F", impersonate_signer);
+        // Aave Oracle contract
+        aave_oracle  = await ethers.getContractAt("AaveOracle" , "0x132C06E86CcCf93Afef7B33f0FF3e2E97EECf8f6", impersonate_signer);
+        //console.log(impersonate_signer.address)
+
+        
     });
 
     describe("Test control over contract", function() {
@@ -26,5 +40,70 @@ describe("Aave-Api3 - Unit tests", function() {
             let owner = await pool_contract.owner();
             expect(owner).to.be.equal(impersonate_signer.address, "Owner must be local signer");
         });
+
+        it("Check Admin role", async function name() {
+            let admin_role = await pool_contract.getACLAdmin()
+            expect(admin_role).to.be.equal(impersonate_signer.address)
+            
+        })
     });
+
+
+    describe("Change chainlink orale for API3 Oracle", function(){
+        it("Update the WETH asset to my custom api3 Oracle" , async function() {
+            let WETH_ADDRESS = "0xD0dF82dE051244f04BfF3A8bB1f62E1cD39eED92"; //Sepolia
+            let new_oracle = await ethers.getContractFactory("WETHapioracle")
+
+            // Here we wrap our contract in a way to make it compatible with aave
+            let sc_oracle = new_oracle.deploy("0x81787D0680b3d4dB1dcb2Cb24D3aE6CCb9c9eBCa") // Our API3sc
+            //console.log((await sc_oracle).address)
+            
+            ;(await sc_oracle).deployed();
+    
+            let old_source =  await aave_oracle.getSourceOfAsset(WETH_ADDRESS)
+            let update_tx =  await aave_oracle.setAssetSources([WETH_ADDRESS] , [(await sc_oracle).address])
+            let new_source = await aave_oracle.getSourceOfAsset(WETH_ADDRESS)
+            expect(new_source).to.not.equal(old_source)
+        
+        })
+        it("Query WETH price with new oracle" , async function () {
+            let WETH_ADDRESS = "0xD0dF82dE051244f04BfF3A8bB1f62E1cD39eED92";
+            let query = await aave_oracle.getAssetPrice(WETH_ADDRESS)
+            let low_limit = ethers.BigNumber.from(1200);
+            let uper_limit = ethers.BigNumber.from(1200);
+            // range [1200 , 2200] this is assuming the fluctations of ether price at 2023-07-22
+            expect(query.gte(low_limit));
+            expect(query.lte(uper_limit));
+        })
+
+    });
+
+    describe("Deploy a Flash Loan", function (){
+        it("Should take a flash loan and be able to return it", async function (){
+            const flashloanexample = await ethers.getContractFactory("FlashLoanExample");
+            let WETH_ADDRESS = "0xD0dF82dE051244f04BfF3A8bB1f62E1cD39eED92";
+
+            let WETH_WHALE = "0x5c4220e10d0D835e9eDf04061379dED26E845bA8"
+            await hre.network.provider.request({
+                method : "hardhat_impersonateAccount",
+                params: [WETH_WHALE],
+            });
+          
+            let weth_whale = await ethers.getSigner(WETH_WHALE);
+            // This is the address of the PoolAddressProvider that can be fond in https://docs.aave.com/developers/deployed-contracts/v3-mainnet/polygon
+            const _flashloanexample = await flashloanexample.connect(weth_whale).deploy(pool_contract.address);
+            await _flashloanexample.deployed();
+            // Fund our contract
+            const token = await ethers.getContractAt("IERC20", WETH_ADDRESS);
+            await token
+            .connect(weth_whale)
+            .transfer(_flashloanexample.address, 1000); // Sending 1000 to the contract
+            const tx = await _flashloanexample.createFlashLoan(WETH_ADDRESS, 10000); // Asking for x10000 more
+            await tx.wait();
+            const remainingBalance = await token.balanceOf(_flashloanexample.address); // Balance of our contract
+            //console.log(remainingBalance)
+            expect(remainingBalance.lt(1000)).to.be.true; // This happens cause we need to give it back with a premium
+        })
+    })
+
 });
